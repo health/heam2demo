e9af42c5caf80f767e1c754451aaa9eaeef99525
health <w21winner@gmail.com>
2012-01-07 14:44:35 +0200

    m2demo commit 070112

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+M2demo::Application.config.secret_token = '1de1aea8d9a74b652b91bf6f82a2b5989b9947cdfb84cbda35daf685e8278f3c718409aca0adc1b82d914c0a886c25c5d6f8461354825374ed0c08edb23032a8'
